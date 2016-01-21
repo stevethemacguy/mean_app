ae38a0c48413047cd2e8d479b564f47ade4ed092
Steven Dunn <Dunn.Steven1@gmail.com>
2016-01-21 14:05:06 -0800

    Added css includes, added bootstrap.css, created a basic angular app.js file.

---
+++ public/app/app.js
@@ -0,0 +1,17 @@
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function($routeProvider, $locationProvider) {
+    //Turns on HTML5 mode for routing
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/',
+            {
+                templateUrl: '/partials/main', //The view to use
+                controller: 'mainCtrl'
+            });
+});
+
+//basic controller
+angular.module('app').controller('mainCtrl', function($scope) {
+    $scope.myVar = "Hello Angular";
+});
--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.use(stylus.middleware(
     }
 ));
 
+//Create a public folder that can be viewable on the server. See http://expressjs.com/en/starter/static-files.html
+//The path you provide is relative to the directory from where you launch node (i.e the folder containing server.controllers)
+//NOTE: The __dirname here IS REQUIRED! Even though it's not initialized to anything!
+app.use(express.static(__dirname + '/public'));     //This makes public the root folder. For example, localhost:3030/favicon.ico would work,
+
 //Create a route that re-routes everything to the index page
 // app.get('/'); //Normally you would use this
 app.get('*', function(request, response)
@@ -43,11 +48,6 @@ app.get('*', function(request, response)
 }); //Since we're using angular, we'll handle routing on the clientside
 //So this will Match all routes...image requests, JS, CSS requests, everything and send them to the index page.
 //This just means you have to be careful to set up your angular routes, including 404, etc.
-
-//Create a public folder that can be viewable on the server. See http://expressjs.com/en/starter/static-files.html
-//The path you provide is relative to the directory from where you launch node (i.e the folder containing server.controllers)
-//NOTE: The __dirname here IS REQUIRED! Even though it's not initialized to anything!
-app.use(express.static(__dirname + '/public'));     //This makes public the root folder. For example, localhost:3030/favicon.ico would work,
 
 var port = 3030;
 app.listen(port, function () {
